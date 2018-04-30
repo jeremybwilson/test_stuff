9f597fe8fdfce5c64a124a25d0362231ca151077
Jeremy Wilson <jeremybwilson@gmail.com>
2018-04-30 09:14:15 -0700

    Console logging for readystate and status messaging

--- destinyapi/scripts.js
+++ destinyapi/scripts.js
@@ -2,6 +2,7 @@ const app = document.getElementById('root');
 
 const logo = document.createElement('img');
 logo.src = 'images/Destiny-Logo.png';
+logo.setAttribute('class', 'logo');
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -15,10 +16,10 @@ var request = new XMLHttpRequest();
 request.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
 request.setRequestHeader("X-API-Key", apiKey);
 
-
 // Begin accessing JSON data here
 request.onreadystatechange = function(){
  if(this.readyState === 4 && this.status === 200){
+  console.log(this.readyState + ', ' + this.status);
 
   var json = JSON.parse(this.responseText);
   console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
